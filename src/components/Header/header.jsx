e7d16c1f2b5a936ddf75c3ea429f2b502c3b5e3d
Sudhanshu-NITR <sudhanshu.kadam.99@gmail.com>
2024-09-01 11:53:05 +0530

    Name Update and Final touchups

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,8 +4,8 @@ function Header() {
     return (
         <div className='w-full'>
             <div className='w-full h-24 flex justify-center items-center'>
-                <a href=""><img src="/header_logo.png" alt="Header Logo" className="h-16 mx-4 flex absolute left-0 top-4"/></a>
-                <h1 className='flex text-[60px] font-exo2 text-white font-semibold'>Club Space</h1>
+                <a href=""><img src="/header_logo.png" alt="Header Logo" className="h-12 md:h-16 lg:h-20 mx-4 flex absolute left-0 top-4"/></a>
+                <h1 className='flex text-[30px] lg:text-[60px] md:text-[44px] font-exo2 text-white font-semibold'>CampusCrew</h1>
             </div>
         </div>
     );
